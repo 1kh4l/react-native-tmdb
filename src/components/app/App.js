7299de305229024a3f4a89fe9d5865e0f00fb4ee
1kh4l <jhon.pantoja@correounivalle.edu.co>
2019-09-14 15:10:46 -0500

    Setting up sass for react native

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,18 +1,27 @@
 import { registerRootComponent } from 'expo';
 import React, { Component } from 'react';
-import './App.scss';
+import styles from './App.scss';
 import { Provider } from 'react-redux';
 import {createStore} from 'redux';
 import reducer from './../../store/reducer';
 import { StyleSheet, Text, View } from 'react-native';
+
+//const styles = StyleSheet.create({
+//  container: {
+//    flex: 1,
+//    backgroundColor: '#fff',
+//    alignItems: 'center',
+//    justifyContent: 'center',
+//  },
+//});
 
 class App extends Component {
   render() {
     const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
     return (
       <Provider store={store}>
-        <View className="App">
-          <Text>Open up App.js to start working on your app!</Text>
+        <View className={styles.App}>
+          <Text className={styles.text}>My first React Native app YAY!</Text>
         </View>
       </Provider>
     );
